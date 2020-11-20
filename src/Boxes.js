a0c895f3a342beb3e33fa741512c22cb67e5f2cc
unknown <jreidmke@gmail.com>
2020-11-20 10:29:54 -0600

    init commit

--- src/Boxes.js
+++ src/Boxes.js
@@ -1,32 +1,35 @@
-import { useState } from 'react';
-import Box from './Box';
-import NewBoxForm from './NewBoxForm';
 
-const Boxes = () => {
-    const [boxes, setBoxes] = useState([]);
 
-    const addBox = (box) => {
-        setBoxes(boxes => [...boxes, box]);
-    }
-    const removeBox = (id) => {
-        setBoxes(boxes => boxes.filter(box => box.id !== id));
-    }
 
-    const newBox = boxes.map(box => (
-        <Box
-        id={box.id}
-        width={box.width}
-        height={box.height}
-        handleRemove={removeBox}
-        backgroundColor={box.color}
-      />
-    ));
-    return (
-        <div>
-            <NewBoxForm createBox={addBox} /> {/* Here we have a form that allows us to add a new box */}
-            {newBox}
-        </div>
-    );
-}
+// import { useState } from 'react';
+// import Box from './Box';
+// import NewBoxForm from './NewBoxForm';
 
-export default Boxes;
+// const Boxes = () => {
+//     const [boxes, setBoxes] = useState([]);
+
+//     const addBox = (box) => {
+//         setBoxes(boxes => [...boxes, box]);
+//     }
+//     const removeBox = (id) => {
+//         setBoxes(boxes => boxes.filter(box => box.id !== id));
+//     }
+
+//     const newBox = boxes.map(box => (
+//         <Box
+//         id={box.id}
+//         width={box.width}
+//         height={box.height}
+//         handleRemove={removeBox}
+//         backgroundColor={box.color}
+//       />
+//     ));
+//     return (
+//         <div>
+//             <NewBoxForm createBox={addBox} /> {/* Here we have a form that allows us to add a new box */}
+//             {newBox}
+//         </div>
+//     );
+// }
+
+// export default Boxes;
