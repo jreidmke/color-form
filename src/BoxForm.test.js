e75612cf80ee21eb5b8048b75c3e49c05e90c1df
unknown <jreidmke@gmail.com>
2020-12-02 11:24:07 -0600

    completed form test

--- src/BoxForm.test.js
+++ src/BoxForm.test.js
@@ -1,4 +1,4 @@
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, getByText } from '@testing-library/react';
 import BoxForm from './BoxForm';
 import Boxes from './Boxes';
 
@@ -12,7 +12,11 @@ it('matches snapshot', () => {
 });
 
 it('allows user to create new box', () => {
-    const {getByText} = render(<Boxes/>);
-    expect(getByText('pink')).not.toBeInTheDocument();
-    
+    const {getByLabelText, queryByText} = render(<Boxes/>);
+    expect(queryByText('pink')).not.toBeInTheDocument();
+    fireEvent.change(getByLabelText('Height'), {target: {value: '100px'}});
+    fireEvent.change(getByLabelText('Width'), {target: {value: '100px'}});
+    fireEvent.change(getByLabelText('Background Color'), {target: {value: 'pink'}});
+    fireEvent.click(queryByText('New Box'));
+    expect(queryByText('pink')).toBeInTheDocument();
 })
